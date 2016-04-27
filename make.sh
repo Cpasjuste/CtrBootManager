c6e510a7dc9c6824e7b63416ff77704f1c95e4e4
Devin Shoemaker <devinshoe@gmail.com>
2016-04-27 10:21:23 -0500

    make.sh: Include clean script + minor hack I decided to throw in the clean script which allows a developer to run make.sh after cloning without needing to run clean.sh. This is very minor but is more convenient for me.
    I also define the cmake toolchain twice which gets around a strange but that prevents the compile from completing. Before this change you could run make.sh twice to get a completed compilation, but this removes the need for that. I would like to figure out why this is needed, but it's not really high on my priority list.

--- make.sh
+++ make.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+./clean.sh &&
 cd build/ &&
+cmake -DCMAKE_TOOLCHAIN_FILE=../DevkitArm3DS.cmake ../ &&
 cmake -DCMAKE_TOOLCHAIN_FILE=../DevkitArm3DS.cmake ../ &&
 cmake --build . --target CtrBootManager_a9lh
